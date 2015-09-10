a6ffc35841db97c3ca98a170c700da3617577921
Martijn van de Rijdt <martijn@enketo.org>
2015-09-10 11:54:22 -0600

    changed: downgraded libxslt, reverting to node 0.12 incompatibility

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -8,7 +8,7 @@ echo 'installing prerequisites...'
 apt-get update
 apt-get upgrade -y
 apt-get install -y curl
-curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash -
+curl -sL https://deb.nodesource.com/setup_0.10 | sudo bash -
 apt-get install -y build-essential nodejs
 
 # installing npm packages
