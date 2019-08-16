2651f63222da8201c8fdaffe547dcfa1c00d95fb
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2019-08-16 14:07:25 +0200

    improve code exmaples in comment

--- src/markdown.js
+++ src/markdown.js
@@ -6,7 +6,7 @@
  * Transforms XForm label and hint textnode content with a subset of Markdown into HTML
  *
  * Supported:
- * - _, __, *, **, [](), #, ##, ###, ####, #####,
+ * - `_`, `__`, `*`, `**`, `[]()`, `#`, `##`, `###`, `####`, `#####`,
  * - span tags and html-encoded span tags,
  * - single-level unordered markdown lists and single-level ordered markdown lists
  * - newline characters
