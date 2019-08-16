7c3000bdcc07d491296ba89c8369a40d0450708e
Leszek Pietrzak <smutnyleszek@gmail.com>
2019-08-16 22:05:03 +0200

    added: documentation (#56)

--- src/language.js
+++ src/language.js
@@ -1,12 +1,25 @@
+/**
+ * @module language
+ */
+
 const tags = require( 'language-tags' );
 const stringDirection = require( 'string-direction' );
 
 /**
+ * @typedef LanguageObj
+ * @property {string} desc
+ * @property {string} tag
+ * @property {string} dir
+ * @property {string} src
+ */
+
+/**
  * Parses a language string into a language object. Guesses missing properties.
  *
- * @see    http://www.iana.org/assignments/language-subtag-registry/language-subtag-registry
- * @param  {string} lang language strings as included in the XForm
- * @return {{desc: string, tag: string, dir: string, src: string}}      language object
+ * @see http://www.iana.org/assignments/language-subtag-registry/language-subtag-registry
+ * @param {string} lang - Language strings as included in the XForm.
+ * @param {string} sample - A text sample.
+ * @return {LanguageObj} language object.
  */
 function parse( lang, sample ) {
     // TODO: this should be refactored
@@ -42,10 +55,10 @@ function parse( lang, sample ) {
 }
 
 /**
- * Performs IANA search to find language object with provided description
- * 
- * @param  {string} desc language description
- * @return {<*>}         the first language object result that was found
+ * Performs IANA search to find language object with provided description.
+ *
+ * @param {string} desc - Language description.
+ * @return {LanguageObj} the first language object result that was found.
  */
 function _getLangWithDesc( desc ) {
     const results = ( desc ) ? tags.search( desc ).filter( _languagesOnly ) : [];
@@ -53,30 +66,30 @@ function _getLangWithDesc( desc ) {
 }
 
 /**
- * Performs IANA search to find language object with provided subtag
- * 
- * @param  {string} tag  subtag
- * @return {<*>}         the first language object result that was found
+ * Performs IANA search to find language object with provided subtag.
+ *
+ * @param  {string} tag - Subtag.
+ * @return {LanguageObj} the first language object result that was found.
  */
 function _getLangWithTag( tag ) {
     return ( tag ) ? tags.subtags( tag ).filter( _languagesOnly )[ 0 ] : '';
 }
 
 /**
- * Filters objects with type "language" from a list of objects
- * 
- * @param  {*} obj language object returned by 'language-tags' library
- * @return {boolean}     whether filter evaluated to true
+ * Filters objects with type "language" from a list of objects.
+ *
+ * @param  {LanguageObj} obj - Language object returned by 'language-tags' library.
+ * @return {boolean} whether filter evaluated to true.
  */
 function _languagesOnly( obj ) {
     return obj.data && obj.data.type === 'language';
 }
 
 /**
- * Obtains the directionality of a text sample
- * 
- * @param  {string} sample a text sample
- * @return {string}     either 'rtl' or the default 'ltr'
+ * Obtains the directionality of a text sample.
+ *
+ * @param {string} sample - A text sample.
+ * @return {string} either 'rtl' or the default 'ltr'.
  */
 function _getDirectionality( sample ) {
     const direction = stringDirection.getDirection( sample );
