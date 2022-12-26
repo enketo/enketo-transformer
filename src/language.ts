25fd47fe8d379e85e8e6d13a974ef105d20761b2
eyelidlessness <trevor.enketo.dev@gmail.com>
2022-12-26 09:55:45 -0800

    Improve naming in language.ts
    While adding types, I found some of these names fairly hard to understand. There's some bikeshedding here in terms of name length, of course, but I do think this helps make more of the code self-documenting

--- src/language.ts
+++ src/language.ts
@@ -3,94 +3,129 @@ import stringDirection from 'string-direction';
 
 import type { Tag, Subtag } from 'language-tags';
 
-class LanguageObj {
+class Language {
+    /**
+     * Included for backwards compatibility, prefer:
+     *
+     * @alias description
+     */
+    get desc() {
+        return this.description;
+    }
+
+    /**
+     * Included for backwards compatibility, prefer:
+     *
+     * @alias directionality
+     */
+    get dir() {
+        return this.directionality;
+    }
+
+    /**
+     * Included for backwards compatibility, prefer:
+     *
+     * @alias sourceLanguage
+     */
+    get src() {
+        return this.sourceLanguage;
+    }
+
     constructor(
-        readonly src: string,
-        readonly desc: string,
+        readonly sourceLanguage: string,
+        readonly description: string,
         readonly tag: string,
-        readonly dir: string
+        readonly directionality: string
     ) {}
 }
 
-export type { LanguageObj };
+export type {
+    Language,
+    /**
+     * Exported for backwards compatibility, prefer:
+     *
+     * @alias Language
+     */
+    Language as LanguageObj,
+};
 
 /**
- * Parses a language string into a language object. Guesses missing properties.
+ * Parses a language string into a {@link Language}. Guesses missing properties.
  * TODO: this should be refactored (more than it has been since this comment was
  * initially written).
  *
  * @see
  * {http://www.iana.org/assignments/language-subtag-registry/language-subtag-registry}
  */
-export const parseLanguage = (lang: string, sample: string): LanguageObj => {
-    const dir = getDirectionality(sample);
-    const src = lang;
+export const parseLanguage = (
+    sourceLanguage: string,
+    sample: string
+): Language => {
+    const directionality = getDirectionality(sample);
 
-    let desc = lang.trim();
-    let tag = desc;
+    let description = sourceLanguage.trim();
+    let tag = description;
 
-    const parts = lang.match(/^([^(]+)\((.*)\)\s*$/);
+    const parts = sourceLanguage.match(/^([^(]+)\((.*)\)\s*$/);
 
     if (parts && parts.length > 2) {
-        desc = parts[1].trim();
+        description = parts[1].trim();
         tag = parts[2].trim();
     } else {
-        // First check whether lang is a known IANA subtag like 'en' or 'en-GB'
-        const langWithTag = getLangWithTag(lang.split('-')[0]);
+        // First check whether `sourceLanguage` is a known IANA subtag like 'en' or 'en-GB'
+        const languageFromTag = getLanguageFromSubtag(
+            sourceLanguage.split('-')[0]
+        );
 
-        if (langWithTag == null) {
-            const { subtag } = getLangWithDesc(desc)?.data ?? {};
+        if (languageFromTag == null) {
+            const { subtag } =
+                getLanguageFromDescription(description)?.data ?? {};
 
             if (subtag != null) {
                 tag = subtag;
             }
         } else {
-            desc = langWithTag.descriptions()[0];
+            description = languageFromTag.descriptions()[0];
         }
     }
 
-    return new LanguageObj(src, desc, tag, dir);
+    return new Language(sourceLanguage, description, tag, directionality);
 };
 
 /**
  * Performs IANA search to find language object with provided description.
- *
- * @param desc - Language description.
- * @return the first language object result that was found.
  */
-const getLangWithDesc = (desc?: string) => {
-    const results: Array<Tag | Subtag> = desc
-        ? tags.search(desc).filter(languagesOnly)
-        : [];
-
-    return results[0] || '';
-};
+const getLanguageFromDescription = (description: string) =>
+    tags.search(description).find(isLanguage);
 
 /**
  * Performs IANA search to find language object with provided subtag.
  */
-const getLangWithTag = (tag: string) =>
-    tag ? tags.subtags(tag).find(languagesOnly) ?? null : null;
+const getLanguageFromSubtag = (subtag: string) =>
+    tags.subtags(subtag).find(isLanguage) ?? null;
 
-/**
- * Filters objects with type "language" from a list of objects.
- *
- * @param  obj - Language object returned by 'language-tags' library.
- * @return whether filter evaluated to true.
- */
-const languagesOnly = (obj: Tag | Subtag) => obj.data.type === 'language';
+const isLanguage = (object: Tag | Subtag) => object.data.type === 'language';
+
+enum Directionality {
+    /**
+     * @default (?)
+     * @see {@link getDirectionality}
+     */
+    LTR = 'ltr',
+    RTL = 'rtl',
+}
 
 /**
  * Obtains the directionality of a text sample.
  *
- * @param sample - A text sample.
- * @return either 'rtl' or the default 'ltr'.
+ * TODO: According to the prior JSDoc, the default is supposed to be
+ * {@link Directionality.LTR}, but the logic suggests the opposite.
  */
 const getDirectionality = (sample: string) => {
     const direction = stringDirection.getDirection(sample);
 
-    if (direction !== 'ltr') {
-        return 'rtl';
+    if (direction !== Directionality.LTR) {
+        return Directionality.RTL;
     }
 
     return direction;
